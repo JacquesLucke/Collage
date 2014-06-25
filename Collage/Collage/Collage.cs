2a954eb8e2a6fe8ca7db42d387160b410d2aef9b
Jacques Lucke <Jacques@moonicfx.com>
2014-06-25 20:18:56 +0200

    add collage class

---
+++ Collage/Collage/Collage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Collage.Collage
+{
+    public class Collage
+    {
+        float aspectRatio = 1.5f;
+
+        public Collage() { }
+    }
+}
