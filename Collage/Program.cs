54e9f0566edded387515f8f39260ebe1b7ad54f6
Jacques Lucke <Jacques@moonicfx.com>
2014-06-25 18:38:17 +0200

    remove unneeded lines from default

--- Collage/Program.cs
+++ Collage/Program.cs
@@ -7,14 +7,8 @@ using System.Linq;
 namespace Collage
 {
 #if WINDOWS || LINUX
-    /// <summary>
-    /// The main class.
-    /// </summary>
     public static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
