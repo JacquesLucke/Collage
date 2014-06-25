af698426870fb9bcc3f217d6e9581a1506a137fd
Jacques Lucke <Jacques@moonicfx.com>
2014-06-25 19:20:25 +0200

    Command class

---
+++ Collage/Undo/Command.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Collage.Undo
+{
+    delegate object DoCommand(object doData);
+    delegate void UndoCommand(object undoData);
+
+    public class Command
+    {
+        DoCommand doCommand;
+        UndoCommand undoCommand;
+
+        object doData, undoData;
+
+        public Command(DoCommand doCommand, UndoCommand undoCommand, object doData)
+        {
+            this.doCommand = doCommand;
+            this.undoCommand = undoCommand;
+            this.doData = doData;
+        }
+
+        public void Execute()
+        {
+            undoData = doCommand(doData);
+        }
+        public void Undo()
+        {
+            undoCommand(undoData);
+        }
+    }
+}
