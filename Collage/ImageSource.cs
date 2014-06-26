bddf6ced4232dbf03eaec9e7ac5362bea55bb3cf
Jacques Lucke <Jacques@moonicfx.com>
2014-06-26 22:54:44 +0200

    minor change

--- Collage/ImageSource.cs
+++ Collage/ImageSource.cs
@@ -20,15 +20,14 @@ namespace Collage
             emptyTexture = new Texture2D(dataAccess.GraphicsDevice, 1, 1);
             emptyTexture.SetData<Color>(new Color[] { Color.White });
         }
-
         public ImageSource(DataAccess dataAccess, int width, int height)
             : this(dataAccess)
         {
             this.width = width;
             this.height = height;
         }
-
-        public ImageSource(DataAccess dataAccess, int width, int height, Color color) : this(dataAccess, width, height)
+        public ImageSource(DataAccess dataAccess, int width, int height, Color color)
+            : this(dataAccess, width, height)
         {
             emptyTexture.SetData<Color>(new Color[] { color });
         }
