f72bca5c4e060f15915f7d2ce57e47819c41b43e
Jacques Lucke <Jacques@moonicfx.com>
2014-06-26 23:07:25 +0200

    new Image class with some positioning code

---
+++ Collage/Image.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Collage
+{
+    public class Image
+    {
+        Vector2 center = new Vector2(0.5f);
+        ImageSource source;
+        float width = 0.1f;
+
+        public Image(ImageSource source)
+        {
+            this.source = source;
+        }
+        public Image(ImageSource source, Vector2 center)
+            : this(source)
+        {
+            this.center = center;
+        }
+
+        public float Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+        public float Height
+        {
+            get { return width / source.AspectRatio; }
+            set { width = value * source.AspectRatio; }
+        }
+
+        public Rectangle GetRectangleInBoundary(Rectangle boundary)
+        {
+            Vector2 realCenter = new Vector2(boundary.Left + center.X * boundary.Width, boundary.Top + center.Y * boundary.Height);
+            float realWidth = boundary.Width * Width;
+            float realHeight = boundary.Height * Height;
+            FloatRectangle realRectangle = new FloatRectangle(realCenter.X - realWidth / 2f, realCenter.Y - realHeight / 2f, realWidth, realHeight);
+            return realRectangle.ToRectangle();
+        }
+    }
+}
