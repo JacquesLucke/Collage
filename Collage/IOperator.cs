b1375f8b41269f1057a431f56e7629f3f296f485
Jacques Lucke <Jacques@moonicfx.com>
2014-06-26 16:46:08 +0200

    interface for operators in general

---
+++ Collage/IOperator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Collage
+{
+    public class IOperator
+    {
+        bool CanStart;
+        bool Start();
+    }
+}
