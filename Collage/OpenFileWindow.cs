210446a2422881607599b6b6b7b6c8ad05bf0979
Jacques Lucke <Jacques@moonicfx.com>
2014-06-27 10:39:30 +0200

    OpenFileWindow
    this commit has some windows specific code. But its only in the class
    OpenFileWindow.

---
+++ Collage/OpenFileWindow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Collage
+{
+    public class OpenFileWindow
+    {
+        public OpenFileWindow() { }
+
+        public string OpenFile(params FileTypes[] fileTypes)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = false;
+            ofd.Filter = Utils.FileTypesToFilter(fileTypes);
+            if (ofd.ShowDialog() == DialogResult.OK) return ofd.FileName;
+            else return null;
+        }
+        public string[] OpenFiles(params FileTypes[] fileTypes)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            ofd.Filter = Utils.FileTypesToFilter(fileTypes);
+            if (ofd.ShowDialog() == DialogResult.OK) return ofd.FileNames;
+            else return null;
+        }
+    }
+}
---
+++ Collage/Utils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Collage
+{
+    public abstract class Utils
+    {
+        public static string FileTypesToFilter(params FileTypes[] types)
+        {
+            string filter = "";
+            for(int i = 0; i< types.Length; i++)
+            {
+                FileTypes type = types[i];
+                switch (type)
+                {
+                    case FileTypes.Images:
+                        filter += "Images|*.jpg;*.png;*.bmp"; break;
+                    case FileTypes.JPG:
+                        filter += "Jpg|*.jpg"; break;
+                    case FileTypes.PNG:
+                        filter += "Png|*.png"; break;
+                    case FileTypes.BMP:
+                        filter += "Bmp|*.bmp"; break;
+                }
+                if (i < types.Length - 1) filter += "|";
+            }
+            return filter;
+        }
+    }
+}
