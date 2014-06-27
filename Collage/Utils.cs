8c675b54ff6afe03a38d03304bcbc2626c55364c
Jacques Lucke <Jacques@moonicfx.com>
2014-06-27 21:20:55 +0200

    new util methods to check if a vector is in a rotated rectangle

--- Collage/Utils.cs
+++ Collage/Utils.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,6 +28,26 @@ namespace Collage
                 if (i < types.Length - 1) filter += "|";
             }
             return filter;
+        }
+
+        public static Vector2 RotateAroundOrigin(Vector2 position, Vector2 origin, float rotation)
+        {
+            return Vector2.Transform((position - origin), Matrix.CreateRotationZ(-rotation)) + origin;
+        }
+        public static bool IsVectorInRotatedRectangle(Vector2 vector, Rectangle rectangle, float rotation)
+        {
+            Vector2 rotatedVector = RotateAroundOrigin(vector, ToVector(rectangle.Center), rotation);
+            return rectangle.Contains(rotatedVector);
+        }
+
+        // convert Vectors and Points
+        public static Point ToPoint(Vector2 vector)
+        {
+            return new Point((int)Math.Round(vector.X), (int)Math.Round(vector.Y));
+        }
+        public static Vector2 ToVector(Point point)
+        {
+            return new Vector2(point.X, point.Y);
         }
     }
 }
