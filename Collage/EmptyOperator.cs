65978388bf4208f70b726ee6bd9379041863cf88
Jacques Lucke <Jacques@moonicfx.com>
2014-06-27 14:47:34 +0200

    EmptyOperator

---
+++ Collage/EmptyOperator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Collage
+{
+    public class example : ICollageOperator
+    {
+        DataAccess dataAccess;
+        CollageEditData editData;
+
+        public example() { }
+
+        public void SetData(DataAccess dataAccess, CollageEditData editData)
+        {
+            this.dataAccess = dataAccess;
+            this.editData = editData;
+        }
+        public bool CanStart()
+        {
+            return false;
+        }
+
+        public bool Start()
+        {
+            return false;
+        }
+    }
+}
