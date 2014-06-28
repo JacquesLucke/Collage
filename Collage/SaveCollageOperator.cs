2484be27381517e03205682cb8f5770225efb181
Jacques Lucke <Jacques@moonicfx.com>
2014-06-28 18:48:01 +0200

    change to SaveCollageOperator
    noticed that this Texture2D can be bigger than 4096x4096 pixel

--- Collage/SaveCollageOperator.cs
+++ Collage/SaveCollageOperator.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using drawing = System.Drawing;
 
 namespace Collage
 {
@@ -32,38 +33,22 @@ namespace Collage
         public bool Start()
         {
             // calculate final dimensions
-            int width = 2000;
+            int width = 8000;
             int height = (int)Math.Round(width / editData.Collage.AspectRatio);
             Rectangle dimensions = new Rectangle(0, 0, width, height);
 
-            // create and set RenderTarget
-            RenderTarget2D rt = new RenderTarget2D(dataAccess.GraphicsDevice, width, height);
-            dataAccess.GraphicsDevice.SetRenderTarget(rt);
-            
-            dataAccess.SpriteBatch.Begin();
-            // draw background color
-            dataAccess.SpriteBatch.Draw(tex, dimensions, editData.Collage.BackgroundColor);
-            // draw each image
-            foreach (Image image in editData.Collage.Images)
-            {
-                // calculate rectangle where the image will be drawn
-                Rectangle imageRectangle = image.GetRectangleInBoundary(dimensions);
-                DrawImageSource(image.Source, imageRectangle, image.Rotation);
-            }
-
-            dataAccess.SpriteBatch.End();
-
-            // reset RenderTarget
-            dataAccess.GraphicsDevice.SetRenderTarget(null);
+            Texture2D render = Render(dimensions, width, height);
 
             // open a dialog to let the user choose the output path
             SaveFileWindow sf = new SaveFileWindow();
             string fileName = sf.SaveFile(FileTypes.JPG, FileTypes.PNG);
             if(fileName != null)
             {
-                System.Drawing.Bitmap bitmap = Utils.ToBitmap(rt);
+                System.Drawing.Bitmap bitmap = Utils.ToBitmap(render);
                 bitmap.Save(fileName);
+                bitmap.Dispose();
             }
+            render.Dispose();
             return false;
         }
 
@@ -73,6 +58,38 @@ namespace Collage
             rectangle.X += rectangle.Width / 2;
             rectangle.Y += rectangle.Height / 2;
             dataAccess.SpriteBatch.Draw(source.Texture, rectangle, null, Color.White, rotation, origin, SpriteEffects.None, 0);
+        }
+
+        private Texture2D Render(Rectangle part, int totalWidth, int totalHeight)
+        {
+            // create and set RenderTarget
+            RenderTarget2D rt = new RenderTarget2D(dataAccess.GraphicsDevice, part.Width, part.Height);
+            dataAccess.GraphicsDevice.SetRenderTarget(rt);
+
+            Rectangle capureRec = new Rectangle(0, 0, part.Width, part.Height);
+            Rectangle totalRec = new Rectangle(-part.X, -part.Y, totalWidth, totalHeight);
+
+            dataAccess.SpriteBatch.Begin();
+            // -----------------------------------------------------------------------------------------
+
+            // draw background color
+            dataAccess.SpriteBatch.Draw(tex, capureRec, editData.Collage.BackgroundColor);
+
+            // draw each image
+            foreach (Image image in editData.Collage.Images)
+            {
+                // calculate rectangle where the image will be drawn
+                Rectangle imageRectangle = image.GetRectangleInBoundary(totalRec);
+                DrawImageSource(image.Source, imageRectangle, image.Rotation);
+            }
+
+            // -----------------------------------------------------------------------------------------
+            dataAccess.SpriteBatch.End();
+
+            // reset RenderTarget
+            dataAccess.GraphicsDevice.SetRenderTarget(null);
+
+            return rt;
         }
     }
 }
