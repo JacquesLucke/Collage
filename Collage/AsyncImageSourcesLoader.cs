d7c4c85ec9086c78db7129ed3bf3b27008014b22
Jacques Lucke <Jacques@moonicfx.com>
2014-06-30 21:22:35 +0200

    simple progress bar for loading images

--- Collage/AsyncImageSourcesLoader.cs
+++ Collage/AsyncImageSourcesLoader.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Gtk;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace Collage
@@ -10,6 +11,8 @@ namespace Collage
     {
         List<ImageSource> imageSources = new List<ImageSource>();
         DataAccess dataAccess;
+        Window window;
+        ProgressBar progressBar;
 
         public AsyncImageSourcesLoader(DataAccess dataAccess, List<ImageSource> sourcesToLoad)
         {
@@ -21,14 +24,35 @@ namespace Collage
         {
             Thread thread = new Thread(LoadingThread);
             thread.Start();
+            dataAccess.GtkThread.Invoke(StartProgressBar);
         }
 
         private void LoadingThread()
         {
+            while (progressBar == null) ;
+            int counter = 0;
             foreach(ImageSource source in imageSources)
             {
+                counter++;
                 source.Load();
+                progressBar.Fraction = (double)counter / (double)imageSources.Count;
+                progressBar.Text = "Load images: " + counter + " of " + imageSources.Count;
             }
+            window.Destroy();
+        }
+
+        private void StartProgressBar()
+        {
+            window = new Window(WindowType.Toplevel);
+            window.Move(10, 10);
+            window.KeepAbove = true;
+            window.Resize(400, 30);
+            progressBar = new ProgressBar();
+            progressBar.Fraction = 0;
+            progressBar.BarStyle = ProgressBarStyle.Continuous;
+            window.Add(progressBar);
+
+            window.ShowAll();
         }
     }
 }
