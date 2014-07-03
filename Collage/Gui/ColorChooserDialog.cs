caa036867c96b36400257820fdfca034644b33ff
Jacques Lucke <Jacques@moonicfx.com>
2014-07-03 21:41:22 +0200

    initial ColorChooserDialog

---
+++ Collage/Gui/ColorChooserDialog.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gtk;
+using Microsoft.Xna.Framework;
+
+namespace Collage
+{
+    public class ColorChooserDialog
+    {
+        ColorSelectionDialog dialog;
+        ResponseType response = ResponseType.None;
+        Color selectedColor;
+
+        public ColorChooserDialog() { }
+
+        public void OpenDialog(Color color)
+        {
+            selectedColor = color;
+
+            dialog = new ColorSelectionDialog("Choose Color");
+            dialog.ColorSelection.CurrentColor = Utils.ToColor(color);
+            dialog.Response += DialogResponse;
+            dialog.Run();
+        }
+
+        private void DialogResponse(object o, ResponseArgs args)
+        {
+            response = args.ResponseId;
+        }
+
+        public Microsoft.Xna.Framework.Color SelectedColor
+        {
+            get
+            {
+                if (dialog != null) selectedColor = Utils.ToColor(dialog.ColorSelection.CurrentColor);
+                return selectedColor;
+            }
+        }
+
+        public ResponseType Response
+        {
+            get { return response; }
+        }
+    }
+}
