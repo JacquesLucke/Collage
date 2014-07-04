0ef65927bb1b49a3ef2b04376ba411f58db1c7f2
Jacques Lucke <Jacques@moonicfx.com>
2014-07-05 01:28:51 +0200

    changes do ScaleOperator close #16

--- Collage/Operators/ScaleOperator.cs
+++ Collage/Operators/ScaleOperator.cs
@@ -37,22 +37,23 @@ namespace Collage
         public bool Update()
         {
             if (dataAccess.Input.ScrollWheelDifference != 0) startTime = DateTime.Now;
-            foreach(Image image in editData.SelectedImages)
+            foreach (Image image in editData.SelectedImages)
             {
                 image.Width *= (dataAccess.Input.ScrollWheelDifference / 2000f) + 1;
                 image.Width = Math.Max(image.Width, 0.01f);
             }
 
-            bool continueScale = (DateTime.Now - startTime).Milliseconds < 300                                                  // stop when the time is up
-                && !dataAccess.Input.IsStrg                                                                                     // stop when the user wants to zoom instead of scale
-                && (!dataAccess.Input.IsLeftButtonDown && editData.SelectedImages.Contains(editData.ImageUnderMouse));         // stop when the user clicks some where no image is
-            if(!continueScale)
+            bool continueScale = (DateTime.Now - startTime).Milliseconds < 300          // stop when the time is up
+                && !dataAccess.Input.IsStrg                                             // stop when the user wants to zoom instead of scale
+                && !(dataAccess.Input.IsLeftButtonDown && (!editData.SelectedImages.Contains(editData.ImageUnderMouse) || editData.ImageUnderMouse == null)); // stop when the user clicks somewhere, where no selection is
+
+            if (!continueScale)
             {
                 float[] newWidth = new float[editData.SelectedImages.Count];
                 for (int i = 0; i < editData.SelectedImages.Count; i++)
-            {
-                newWidth[i] = editData.SelectedImages[i].Width;
-            }
+                {
+                    newWidth[i] = editData.SelectedImages[i].Width;
+                }
                 Command command = new Command(ExecuteScale, ExecuteScale, newWidth, "Scale Selected Images");
                 command.SetUndoData(startWidth);
                 editData.UndoManager.AddCommand(command);
